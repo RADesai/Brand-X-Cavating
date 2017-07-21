6577e45f7e4585b529aab27a294f897af1ee8ba6
Raj Desai <rajdesai94@gmail.com>
2017-07-20 22:36:50 -0400

    assets file

---
+++ src/data/assets.js
@@ -0,0 +1,13 @@
+const services = [
+  [ 'Retaining Walls', 'https://image.ibb.co/nQQwTv/brick_wall.png' ],
+  [ 'Demolition', 'https://image.ibb.co/cVVVov/demolition_crane.png' ],
+  [ 'Septic Systems', 'https://image.ibb.co/dZrQMF/tanks.png' ],
+  [ 'Hammer', 'https://image.ibb.co/bV46Tv/boring_machine.png' ],
+  [ 'Grapple Service', 'https://image.ibb.co/eqt0ov/truck_crane.png' ],
+  [ 'Trucking', 'https://image.ibb.co/bxsRTv/truck.png' ],
+  [ 'Snow Plowing', 'https://image.ibb.co/kKDi8v/truck_cleaner_snow.png' ],
+  [ 'Drainage', 'https://image.ibb.co/isq8aa/faucet.png' ],
+  [ 'Mobile Crushing', 'https://image.ibb.co/cAMmTv/excavator.png']
+];
+
+export default services;
