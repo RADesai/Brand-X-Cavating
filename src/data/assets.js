28660e08893527132cc1bfc8e638768bd539a177
Raj Desai <rajdesai94@gmail.com>
2017-07-21 16:19:23 -0400

    assets refactoring with service descriptions

--- src/data/assets.js
+++ src/data/assets.js
@@ -1,13 +1,49 @@
 const services = [
-  [ 'Retaining Walls', 'https://image.ibb.co/nQQwTv/brick_wall.png' ],
-  [ 'Demolition', 'https://image.ibb.co/cVVVov/demolition_crane.png' ],
-  [ 'Septic Systems', 'https://image.ibb.co/dZrQMF/tanks.png' ],
-  [ 'Hammer', 'https://image.ibb.co/bV46Tv/boring_machine.png' ],
-  [ 'Grapple Service', 'https://image.ibb.co/eqt0ov/truck_crane.png' ],
-  [ 'Trucking', 'https://image.ibb.co/bxsRTv/truck.png' ],
-  [ 'Snow Plowing', 'https://image.ibb.co/kKDi8v/truck_cleaner_snow.png' ],
-  [ 'Drainage', 'https://image.ibb.co/isq8aa/faucet.png' ],
-  [ 'Mobile Crushing', 'https://image.ibb.co/cAMmTv/excavator.png']
+  {
+    icon: 'https://image.ibb.co/nQQwTv/brick_wall.png',
+    name: 'Retaining Walls',
+    description: 'Feel free to contact us by phone or through the contact page of the site, we look forward to working with you!'
+  },
+  {
+    icon: 'https://image.ibb.co/cVVVov/demolition_crane.png',
+    name: 'Demolition',
+    description: 'Feel free to contact us by phone or through the contact page of the site, we look forward to working with you!'
+  },
+  {
+    icon: 'https://image.ibb.co/dZrQMF/tanks.png',
+    name: 'Septic Systems',
+    description: 'Feel free to contact us by phone or through the contact page of the site, we look forward to working with you!'
+  },
+  {
+    icon: 'https://image.ibb.co/bV46Tv/boring_machine.png',
+    name: 'Hammer',
+    description: 'Feel free to contact us by phone or through the contact page of the site, we look forward to working with you!'
+  },
+  {
+    icon: 'https://image.ibb.co/eqt0ov/truck_crane.png',
+    name: 'Grapple Service',
+    description: 'Feel free to contact us by phone or through the contact page of the site, we look forward to working with you!'
+  },
+  {
+    icon: 'https://image.ibb.co/bxsRTv/truck.png',
+    name: 'Trucking',
+    description: 'Feel free to contact us by phone or through the contact page of the site, we look forward to working with you!'
+  },
+  {
+    icon: 'https://image.ibb.co/kKDi8v/truck_cleaner_snow.png',
+    name: 'Snow Plowing',
+    description: 'Feel free to contact us by phone or through the contact page of the site, we look forward to working with you!'
+  },
+  {
+    icon: 'https://image.ibb.co/isq8aa/faucet.png',
+    name: 'Drainage',
+    description: 'Feel free to contact us by phone or through the contact page of the site, we look forward to working with you!'
+  },
+  {
+    icon: 'https://image.ibb.co/cAMmTv/excavator.png',
+    name: 'Mobile Crushing',
+    description: 'Feel free to contact us by phone or through the contact page of the site, we look forward to working with you!'
+  }
 ];
 
 export default services;
